61b9cbeb70c3d4d2f7060fb7ccbec3edc6732013
Zachary Cutlip <uid000@gmail.com>
2019-07-05 13:05:59 -0700

    add about() and version() to __init__.py

--- pyobjfile/__init__.py
+++ pyobjfile/__init__.py
@@ -1,8 +1,18 @@
-from grumpy.__about__ import (
+from pyobjfile.__about__ import (
     __version__,
     __title__,
     __summary__
 )
+
+
+def version():
+    return "%s %s" % (__title__, __version__)
+
+
+def about():
+    return ("%s\n%s" % (version(), __summary__))
+
+
 __all__ = [
     "__version__",
     "__title__",
