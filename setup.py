53b6cb71a8a40dd617cfae8e56747c366c9a778d
Zachary Cutlip <uid000@gmail.com>
2019-08-30 12:09:11 -0700

    depend on py-dwarf >= 0.2.0.dev0

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name=about["__title__"],
       url="https://github.com/zcutlip/py-object-file",
       packages=find_packages(),
       python_requires='>=2.7',
-      install_requires=["py-dwarf", "future"]
+      install_requires=["py-dwarf>=0.2.0.dev0", "future"]
       )
